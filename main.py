73fe69e6423cc9324f666908a5a3dfce14ca82da
Brendan <ulxlx71@aim.com>
2018-03-23 09:08:23 -0400

    players can have multiple pieces

--- main.py
+++ main.py
@@ -13,17 +13,35 @@ board_size = board_width, board_height = 800, 800
 square_size = board_width/10
 
 turn = 0
+all_pieces = []
+active_piece = None
 
 class Player:
-	speed = 4
-	def __init__(self, loc, img):
+	def __init__(self):
+		self.pieces = []
+
+	def addPieces(self, pieces):
+		for piece in pieces:
+			all_pieces.append(piece)
+			self.pieces.append(piece)
+
+	def getPieces(self):
+		return self.pieces
+
+class Piece:
+	def __init__(self, loc, speed, img):
 		self.loc = loc
+		self.speed = speed
 		self.img = img
 
 def getDist(loc1, loc2):
 	return math.sqrt(math.pow((loc1[0]-loc2[0]),2)+math.pow((loc1[1]-loc2[1]),2))
 
-players = [Player((1,1), player1_img), Player((6,5), player2_img)]
+players = [Player(), Player()]
+
+players[0].addPieces([Piece((1,1), 2, player1_img), Piece((1,3), 2, player1_img)])
+players[1].addPieces([Piece((7,8), 2, player2_img), Piece((6,7), 2, player2_img)])
+
 
 done = False
 while not done:
@@ -31,12 +49,23 @@ while not done:
 		if event.type == pygame.QUIT:
 			done = True
 		elif event.type == pygame.MOUSEBUTTONDOWN:
+			move = True
 			mouse_loc = mousex, mousey = pygame.mouse.get_pos()
 			click_loc = click_x, click_y = mousex % board_width/square_size, mousey % board_height/square_size
-			if getDist(players[turn].loc, click_loc) <= players[turn].speed:
-				players[turn].loc = click_loc
-				turn = (turn+1)%len(players)
-				print turn
+			for piece in players[turn].getPieces():
+				if(getDist(piece.loc, click_loc) == 0):
+					print piece
+					active_piece = piece
+					move = False
+			if move == True and active_piece is not None and getDist(active_piece.loc, click_loc) <= active_piece.speed:
+				is_open = True
+				for piece in all_pieces:
+					if click_loc == piece.loc:
+						is_open = False
+				if is_open:
+					active_piece.loc = click_loc
+					turn = (turn+1)%len(players)
+					active_piece = None
 
 	screen.fill((0,0,0))
 
@@ -46,10 +75,12 @@ while not done:
 
 	for x in range(10):
 		for y in range(10):
-			if(getDist(players[turn].loc, (x, y)) <= players[turn].speed and not (x is players[turn].loc[0] and y is players[turn].loc[1])):
-				pygame.draw.circle(screen, (0,0,255), (x*square_size+(square_size/2), y*square_size+(square_size/2)), square_size/2)
+			if active_piece != None:
+				if getDist(active_piece.loc, (x, y)) <= active_piece.speed and not (x is active_piece.loc[0] and y is active_piece.loc[1]):
+					pygame.draw.circle(screen, (0,0,255), (x*square_size+(square_size/2), y*square_size+(square_size/2)), square_size/2)
 
 	for player in players:
-		screen.blit(player.img, (player.loc[0]*square_size+10, player.loc[1]*square_size+10))
+		for piece in player.pieces:
+			screen.blit(piece.img, (piece.loc[0]*square_size+10, piece.loc[1]*square_size+10))
 
 	pygame.display.flip()
