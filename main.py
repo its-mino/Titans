e229b1a00f2de02a725bef44349bc2eca3cac453
Brendan <ulxlx71@aim.com>
2018-03-23 17:14:20 -0400

    can now activate attacks

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
 import math
+import json
 
 pygame.init()
+pygame.font.init()
+
 size = width, height = 1000,800
 screen = pygame.display.set_mode((size))
+font = pygame.font.SysFont('Calibri', 30)
 
-player1_img = pygame.image.load('img/player.jpg')
-player2_img = pygame.image.load('img/player2.jpg')
+piece_imgs = {'melee': pygame.image.load('img/melee.jpg'), 'ranged': pygame.image.load('img/ranged.jpg')}
+
+piece_types = json.load(open('templates/piece_types.json'))
+attacks = json.load(open('templates/attacks.json'))
+active_attack = None
+attack_buttons = []
 
 board_size = board_width, board_height = 800, 800
 
@@ -29,18 +37,21 @@ class Player:
 		return self.pieces
 
 class Piece:
-	def __init__(self, loc, speed, img):
+	def __init__(self, loc, template):
 		self.loc = loc
-		self.speed = speed
-		self.img = img
+		self.health = piece_types[template]['health']
+		self.speed = piece_types[template]['speed']
+		self.attacks = piece_types[template]['attacks']
+		self.img = piece_imgs[template]
+
 
 def getDist(loc1, loc2):
 	return math.sqrt(math.pow((loc1[0]-loc2[0]),2)+math.pow((loc1[1]-loc2[1]),2))
 
 players = [Player(), Player()]
 
-players[0].addPieces([Piece((1,1), 2, player1_img), Piece((1,3), 2, player1_img)])
-players[1].addPieces([Piece((7,8), 2, player2_img), Piece((6,7), 2, player2_img)])
+players[0].addPieces([Piece((1,1), 'melee'), Piece((1,3), 'ranged')])
+players[1].addPieces([Piece((7,8), 'melee'), Piece((6,7), 'ranged')])
 
 
 done = False
@@ -50,22 +61,45 @@ while not done:
 			done = True
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			move = True
-			mouse_loc = mousex, mousey = pygame.mouse.get_pos()
-			click_loc = click_x, click_y = mousex % board_width/square_size, mousey % board_height/square_size
-			for piece in players[turn].getPieces():
-				if(getDist(piece.loc, click_loc) == 0):
-					print piece
-					active_piece = piece
-					move = False
-			if move == True and active_piece is not None and getDist(active_piece.loc, click_loc) <= active_piece.speed:
-				is_open = True
-				for piece in all_pieces:
-					if click_loc == piece.loc:
-						is_open = False
-				if is_open:
-					active_piece.loc = click_loc
-					turn = (turn+1)%len(players)
-					active_piece = None
+			mouse_loc = mouse_x, mouse_y = pygame.mouse.get_pos()
+			button_clicked = False
+			for button in attack_buttons:
+				if button[2].collidepoint(mouse_loc):
+					if(active_attack == button[1]):
+						active_attack = None
+					else:
+						active_attack = button[1]
+					button_clicked = True
+			if not button_clicked:
+				click_loc = click_x, click_y = mouse_x % board_width/square_size, mouse_y % board_height/square_size
+				for piece in players[turn].getPieces():
+					if(getDist(piece.loc, click_loc) == 0):
+						if(piece is active_piece):
+							active_piece = None
+							active_attack = None
+							attack_buttons = []
+						else:
+							attack_buttons = []
+							active_piece = piece
+							active_attack = None
+							for i in range(len(active_piece.attacks)):
+								attack_text = font.render(active_piece.attacks[i], False, (255, 255, 255))
+								button = pygame.Rect(board_width+10, 210*i, width-board_width-20, 100)
+								pygame.draw.rect(screen, (255, 0, 255), button)
+								text_rect = attack_text.get_rect()
+								text_rect.center = button.center
+								screen.blit(attack_text, text_rect)
+								attack_buttons.append((attack_text, active_piece.attacks[i], button, text_rect))
+						move = False
+				if move == True and active_attack == None and active_piece is not None and getDist(active_piece.loc, click_loc) <= active_piece.speed:
+					is_open = True
+					for piece in all_pieces:
+						if click_loc == piece.loc:
+							is_open = False
+					if is_open:
+						active_piece.loc = click_loc
+						turn = (turn+1)%len(players)
+						active_piece = None
 
 	screen.fill((0,0,0))
 
@@ -76,11 +110,19 @@ while not done:
 	for x in range(10):
 		for y in range(10):
 			if active_piece != None:
-				if getDist(active_piece.loc, (x, y)) <= active_piece.speed and not (x is active_piece.loc[0] and y is active_piece.loc[1]):
-					pygame.draw.circle(screen, (0,0,255), (x*square_size+(square_size/2), y*square_size+(square_size/2)), square_size/2)
+				if active_attack != None:
+					if getDist(active_piece.loc, (x, y)) <= attacks[active_attack]['range'] and not (x is active_piece.loc[0] and y is active_piece.loc[1]):
+						pygame.draw.circle(screen, (0,255,0), (x*square_size+(square_size/2), y*square_size+(square_size/2)), square_size/2-5)
+				elif getDist(active_piece.loc, (x, y)) <= active_piece.speed and not (x is active_piece.loc[0] and y is active_piece.loc[1]):
+					pygame.draw.circle(screen, (0,0,255), (x*square_size+(square_size/2), y*square_size+(square_size/2)), square_size/2-5)
 
 	for player in players:
 		for piece in player.pieces:
 			screen.blit(piece.img, (piece.loc[0]*square_size+10, piece.loc[1]*square_size+10))
+
+	if(active_piece != None):
+		for button in attack_buttons:
+			pygame.draw.rect(screen, (255, 0, 255), button[2])
+			screen.blit(button[0], button[3])
 
 	pygame.display.flip()
